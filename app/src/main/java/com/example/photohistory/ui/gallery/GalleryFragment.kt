222d5910baaaec19d95ae60a3f179351bd17c63a
MaksimFurgala <furgala.maxim@yandex.ru>
2024-06-19 23:03:57 +0300

    added: - resources update: - fragment_gallery.xml; - ui logic for creating new photo; - GalleryViewModel; - AndroidManifest.xml.

--- app/src/main/java/com/example/photohistory/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/photohistory/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,14 @@
 package com.example.photohistory.ui.gallery
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.photohistory.databinding.FragmentGalleryBinding
@@ -12,10 +17,27 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class GalleryFragment : Fragment() {
     private var _binding: FragmentGalleryBinding? = null
-
     private val binding get() = _binding!!
 
     val galleryViewModel by viewModels<GalleryViewModel>()
+
+    private lateinit var contractImageCapture: ActivityResultLauncher<Uri>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // Регистрируем новый контракт Камера и инициализируем обработчик результата.
+        contractImageCapture = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            if (it) {
+                binding.ivNewPhoto.setImageURI(galleryViewModel.newImageUri.value)
+                binding.tvNameNewPhoto.text = galleryViewModel.newImageUri.value?.lastPathSegment ?: ""
+            } else {
+                // В случае отмены логируем и удаляем временный файл.
+                Log.d("GalleryFragment", "ImageCapture is cancelled.")
+                galleryViewModel.newImageFile.value?.delete()
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,15 +45,35 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         with(binding) {
             lifecycleOwner = viewLifecycleOwner
             viewModel = galleryViewModel
+
+            // Создаем обработчик события создания нового фото: создание нового Uri и запуск контракта.
+            fabCreatePhoto.setOnClickListener {
+                galleryViewModel.createImageUri()
+                contractImageCapture.launch(galleryViewModel.newImageUri.value)
+            }
+        }
+
+        observeViewModel()
+    }
+
+    /**
+     * Подписка на изменения объектов LiveData из ViewModel.
+     *
+     */
+    private fun observeViewModel() {
+        with(galleryViewModel) {
+            newImageUri.observe(viewLifecycleOwner) {
+                binding.ivNewPhoto.setImageURI(it)
+            }
         }
     }
 
--- app/src/main/java/com/example/photohistory/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/photohistory/ui/gallery/GalleryViewModel.kt
@@ -1,18 +1,21 @@
 package com.example.photohistory.ui.gallery
 
-import android.content.ActivityNotFoundException
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
-import android.provider.MediaStore
-import androidx.activity.result.ActivityResultLauncher
+import android.os.Environment
+import android.util.Log
+import androidx.core.content.FileProvider
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.photohistory.domain.usecases.PhotoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,5 +23,55 @@ class GalleryViewModel @Inject constructor(@ApplicationContext val context: Cont
 
     @Inject
     lateinit var photoUseCase: PhotoUseCase
+
+    private val _newImageUri = MutableLiveData<Uri>()
+    val newImageUri: LiveData<Uri>
+        get() = _newImageUri
+
+    private val _newImageFile = MutableLiveData<File?>()
+    val newImageFile: LiveData<File?>
+        get() = _newImageFile
+
+    /**
+     * Создание Uri для файла новой фотографии и обновление свойств для Uri и File.
+     *
+     * @return Uri файла
+     */
+    fun createImageUri(): Uri {
+        // Создаем временный файл для сохранения фото
+        val photoFile: File? = try {
+            createImageFile()
+        } catch (ex: IOException) {
+            Log.e("GalleryViewModel", "Error creating tempFile.")
+            null
+        }
+
+        // Получаем Uri для файла и обновляем значения для объектов liveData.
+        return FileProvider.getUriForFile(
+            context,
+            "com.example.photohistory.FileProvider",
+            photoFile!!
+        ).also {
+            _newImageUri.value = it
+            _newImageFile.value = photoFile
+        }
+    }
+
+    /**
+     * Создание файла для нового фото.
+     *
+     * @return файл
+     */
+    @SuppressLint("SimpleDateFormat")
+    private fun createImageFile(): File {
+        // Создаем уникальное имя файла на основе даты и времени
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        )
+    }
 
 }
